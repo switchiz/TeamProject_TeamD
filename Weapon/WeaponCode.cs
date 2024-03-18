705a062f633f6ef8b7c9c6fb2ab59d440f7f31ba
gungkim <154861338+gungkim@users.noreply.github.com>
2024-03-18 15:16:33 +0900

    2024.03.18_김경백
    weaponData, weaponcode

---
+++ Weapon/WeaponCode.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum WeaponCode
+{
+    ShortSword =0,
+    Katana,
+    WoodenSword,
+
+}
+
+public enum SortBy
+{
+    Code,
+    Name,
+    Price,
+}
